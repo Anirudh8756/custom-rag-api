56c056574ffcba2bb23b4e711c25abc14b55da9a
Anirudh Das <dasanirudh5@gmail.com>
2024-11-04 18:00:52 +0530

    add user files to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,12 +4,23 @@ class DashboardController < ApplicationController
     def index 
         auth_token = Warden::JWTAuth::UserEncoder.new.call(
             current_user, :user, nil
-          ).first
+          ).first 
+        latest_files = current_user.upload_blobs.order(created_at: :DESC).limit(5)
+        file_names = []
+        if latest_files.present? 
+          latest_files.each do |latest_file|
+            file_names << latest_file.filename.to_s
+          end
+        else 
+          
+        end
+
       render json: {
         user_email: current_user.email,
         user_id: current_user.id,
-        authentication_token: auth_token
+        user_files: file_names
       }
+     
     end
     private
     
