c1830d622373d9c57db2f7d8703e66e519d59c74
Anirudh Das <dasanirudh5@gmail.com>
2024-11-11 16:26:50 +0530

    route fix and upload_type_validatation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
   has_many_attached :upload
-
-  validates :upload ,  content_type: ['application/pdf']
+  validates :upload, attached: true, content_type: [
+  'application/pdf',
+  'application/msword',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  'application/vnd.ms-excel',
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  'application/vnd.ms-powerpoint',
+  'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+  'text/plain'
+]
+  validates_size_of :upload, maximum: 25.megabytes, message: "should be less than 25MB"
   has_many :messages , dependent: :destroy
   has_many :faqs, dependent: :destroy
   devise :database_authenticatable, :registerable,
@@ -12,4 +21,5 @@ class User < ApplicationRecord
   def jwt_payload
     super
   end
+  
 end
